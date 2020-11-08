e56a3ac548ae6e9007bab5b6f94a3facd787c328
Owais Naqvi <65233517+OwaisHaider896@users.noreply.github.com>
2020-11-08 19:24:55 +0500

    recieve data from API

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,31 @@ import React, { useState } from "react";
 
 const Home = () => {
   const [user, searchUser] = useState("");
+  const [userData, setUserData] = useState([]);
+
+  const ApiKey = "AIzaSyACI5bTAVfUubIPeb_sP2tOSrRyKnCZRdA";
+
+  const click = async () => {
+    const response = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${user}&key=${ApiKey}`
+    );
+    const data = await response.json();
+    setUserData(data);
+
+    console.log(data);
+  };
 
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        value={user}
+        onChange={(e) => {
+          searchUser(e.target.value);
+        }}
+      />
 
-      <button>Search</button>
+      <button onClick={click}>Search</button>
     </div>
   );
 };
